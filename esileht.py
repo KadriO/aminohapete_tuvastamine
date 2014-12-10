c5cf0c3c9035e6eacd4a51c28e1f12c05ae4bf46
KadriO <kadri_756@hotmail.com>
2014-12-10 23:15:34 +0200

    Lihtne aken, et kontrollida funktsioonide töötamist

---
+++ esileht.py
@@ -0,0 +1,28 @@
+__author__ = 'Kadri'
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+
+raam = Tk()
+raam.title("Aminohapete mäng")
+tahvel = Canvas(raam, width= 550, height= 300, background= "white")
+tahvel.grid()
+
+suur_font = font.Font(family='Times', size=14, weight='bold')
+
+silt1 = ttk.Label(raam, text="Oled avanud aminohapete mängu!", font=suur_font, background= "white")
+silt1.place(x=110,y=1)
+silt2 = ttk.Label(raam, text= "Mängida on võimalik erinevatel raskustasemetel. Kui soovid valida valikvastustega versiooni, "
+                              , background= "white")
+silt2.place(x=30, y=30)
+silt3 = ttk.Label(raam, text= "vajuta nuppu xxx. Soovides mängida xxx, vajuta nupule xxx", background="white")
+silt3.place(x=80, y=50)
+
+vasak_nupp = ttk.Button(raam, text="Vajuta mind")
+vasak_nupp.place(x= 50, y= 150, width=100)
+
+vasak_nupp = ttk.Button(raam, text="Vajuta mind ka")
+vasak_nupp.place(x= 200, y= 150, width=100)
+
+raam.mainloop()
