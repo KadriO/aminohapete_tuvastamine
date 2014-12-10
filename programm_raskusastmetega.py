80b97e35ecbff5a321070495d5eece74e0461499
AhtoS <ahtosalumets@hotmail.com>
2014-12-10 10:41:49 +0200

    Panin raskusastmed ja programmi hetkel kokku ning tõstsin nad enamvähem paika, hetkel veel ei tööta nii nagu peab. Nt ei kuva aminohapete pilte jms, samas akende enda vahetus on ok.

--- programm_raskusastmetega.py
+++ programm_raskusastmetega.py
@@ -2,80 +2,8 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 #from tkinter import messagebox
+from random import randint
 
-#Raskuksastmed, kõige raskema puhul vihjeid ei pane, kergemal tasemel on vihjeks/
-#aminohappe kolmetäheline lühend, keskmisel ühetäheline
-
-sonastik_lihtsam ={"alaniin": "Ala", "arginiin": "Arg", "asparagiin": "Asn"
-    , "asparagiinhape": "Asp", "Glutamiin": "Gln", "Glutamiinhape": "Glu",
-    "glütsiin": "Gly", "histidiin": "His", "isoleutsiin": "Ile", "leutsiin": "Leu",
-    "lüsiin": "Lys", "metioniin": "Met", "fenüülalaniin": "Phe", "proliin": "Pro",
-    "seriin": "Ser", "treoniin": "Thr", "trüptofaan": "Trp", "tsüsteiin": "Cys",
-    "türosiin": "Tyr", "valiin": "Val", "selenotsüsteiin": "Sec", "pürrolüsiin": "Pyl"}
-sonastik_keskmine = {"alaniin": "A", "arginiin": "R", "asparagiin": "N",
-    "asparagiinhape": "D", "glutamiin": "Q", "glutamiinhape": "E", "glütsiin": "G",
-    "Histidiin": "H", "Isoleutsiin": "I", "Leutsiin": "L", "Lüsiin": "K", "Metioniin": "M",
-    "fenüülalaniin": "F", "proliin": "P", "seriin": "S", "treoniin": "T", "trüptofaan": "W",
-    "tsüsteiin": "C", "türosiin": "Y", "valiin": "V", "selenotsüsteiin": "U",
-                   "pürrolüsiin": "O"}
-
-# see funktsioon käivitatakse nupule klõpsamisel
-def raskusaste():
-    if v.get()== 1:
-        print("kas töötab?")
-        sonastik = sonastik_lihtsam
-        print(sonastik)
-    elif v.get() == 2:
-        print("vist töötab")
-        sonastik = sonastik_keskmine
-        print(sonastik)
-    elif v.get() == 3:
-        return
-        #ei muuda midagi
-    #teen tahvli tühjaks
-    tahvel.delete("all")
-    nupp.place_forget()
-    global nupp1
-    global nupp2
-    global nupp3
-    nupp1.destroy()
-    nupp2.destroy()
-    nupp3.destroy()
-
-# loome akna
-raam = Tk()
-raam.title("Aminohapete mäng")
-#raam.geometry("550x300")
-tahvel = Canvas(raam, width= 550, height= 300)
-tahvel.grid()
-
-#aminohappe pilt tahvlile
-#normaal_suurus = Image.open("pildid/aminohape.png")
-#muudetud = normaal_suurus.resize((200,150),Image.ANTIALIAS)
-#aminohape = ImageTk.PhotoImage(muudetud)
-#pilt = tahvel.create_image(175, 100, anchor = W, image= aminohape)
-
-#radiobutton
-v = IntVar()
-#IntVar (või StringVar) väärtuseks on hetkel valitud radiobuttoni value.
-#Selle järgi saab kindlaks teha, milline on valitud.
-#Submit nupp on tavaline nupp. Peale submit vajutamist kontrollid IntVar väärtust.
-
-#Iga nupp on kahel real, et saaksin kasutada destroy() käsku
-nupp1 = Radiobutton(raam, text="Kerge", variable=v, value=1)
-nupp1.grid(row=1)
-nupp2 = Radiobutton(raam, text="Keskmine", variable=v, value=2)
-nupp2.grid(row=2)
-nupp3 = Radiobutton(raam, text="Raske", variable=v, value=3)
-nupp3.grid(row=3)
-
-# loome nupu
-nupp = ttk.Button(raam, text="Valmis", command=raskusaste)
-#Button(raam, text="Tervita!", command=lambda: tervita(argument))
-nupp.place(x=200, y=200, width=150)
-
-# ilmutame akna ekraanile
-raam.mainloop()
 def fn_kontrolli(sisestus, ah_nimi):
     vastus = False
     if sisestus == ah_nimi:
@@ -123,35 +51,107 @@ def salvesta():
     print(tulemused)
     vaheta_aminohape()
 
-#Programm
-raam = Tk()
-raam.title("Aminohapete mäng")
-tahvel = Canvas(raam, width= 550, height= 300, background = "white")
-tahvel.grid()
-
-aminohapped = ["alaniin", "arginiin", "asparagiin", "asparagiinhape", "tsüsteiin", "glutamiin", "glutamiinhape", "glütsiin",
+def põhi_programm():
+    aminohapped = ["alaniin", "arginiin", "asparagiin", "asparagiinhape", "tsüsteiin", "glutamiin", "glutamiinhape", "glütsiin",
 "histidiin", "isoleutsiin", "leutsiin", "lüsiin", "metioniin", "fenüülalaniin", "proliin", "seriin",
 "treoniin", "trüptofaan", "türosiin", "valiin", "selenotsüsteiin", "pürrolüsiin"]
 
-tulemused = []
+    tulemused = []
 
-ah_number = randint(0, len(aminohapped)-1)
-ah_nimi = aminohapped[ah_number]
-print(ah_nimi)
-print(ah_number)
+    ah_number = randint(0, len(aminohapped)-1)
+    ah_nimi = aminohapped[ah_number]
+    print(ah_nimi)
+    print(ah_number)
 
-normaal_suurus = Image.open("pildid/"+ah_nimi+".png")
-muudetud = normaal_suurus.resize((200,150),Image.ANTIALIAS)
-aminohape = ImageTk.PhotoImage(muudetud)
+    normaal_suurus = Image.open("pildid/"+ah_nimi+".png")
+    muudetud = normaal_suurus.resize((200,150),Image.ANTIALIAS)
+    aminohape = ImageTk.PhotoImage(muudetud)
 
-pildi_id = tahvel.create_image(30, 130, anchor = W, image= aminohape)
+    pildi_id = tahvel.create_image(30, 130, anchor = W, image= aminohape)
 
 #sildi tegemine:
-silt = Label(raam, background="white", text="Kirjuta aminohappe nimi:")
-silt.place(x=300, y=110)
+    silt = Label(raam, background="white", text="Kirjuta aminohappe nimi:")
+    silt.place(x=300, y=110)
 #tekstikasti tegemine:
-ah_lahter_id = ttk.Entry(raam)
-ah_lahter_id.place(x=300, y=130, width=150)
+    ah_lahter_id = ttk.Entry(raam)
+    ah_lahter_id.place(x=300, y=130, width=150)
 #nupp:
-nupp = ttk.Button(raam, text="Salvesta vastus", command = salvesta)
-nupp.place(x=330, y=170, width=100)
+    nupp = ttk.Button(raam, text="Salvesta vastus", command = salvesta)
+    nupp.place(x=330, y=170, width=100)
+
+#Raskusastmed, kõige raskema puhul vihjeid ei pane, kergemal tasemel on vihjeks/
+#aminohappe kolmetäheline lühend, keskmisel ühetäheline
+
+sonastik_lihtsam ={"alaniin": "Ala", "arginiin": "Arg", "asparagiin": "Asn"
+    , "asparagiinhape": "Asp", "Glutamiin": "Gln", "Glutamiinhape": "Glu",
+    "glütsiin": "Gly", "histidiin": "His", "isoleutsiin": "Ile", "leutsiin": "Leu",
+    "lüsiin": "Lys", "metioniin": "Met", "fenüülalaniin": "Phe", "proliin": "Pro",
+    "seriin": "Ser", "treoniin": "Thr", "trüptofaan": "Trp", "tsüsteiin": "Cys",
+    "türosiin": "Tyr", "valiin": "Val", "selenotsüsteiin": "Sec", "pürrolüsiin": "Pyl"}
+sonastik_keskmine = {"alaniin": "A", "arginiin": "R", "asparagiin": "N",
+    "asparagiinhape": "D", "glutamiin": "Q", "glutamiinhape": "E", "glütsiin": "G",
+    "Histidiin": "H", "Isoleutsiin": "I", "Leutsiin": "L", "Lüsiin": "K", "Metioniin": "M",
+    "fenüülalaniin": "F", "proliin": "P", "seriin": "S", "treoniin": "T", "trüptofaan": "W",
+    "tsüsteiin": "C", "türosiin": "Y", "valiin": "V", "selenotsüsteiin": "U",
+                   "pürrolüsiin": "O"}
+
+# see funktsioon käivitatakse nupule klõpsamisel
+def raskusaste():
+    if v.get()== 1:
+        print("kas töötab?")
+        sonastik = sonastik_lihtsam
+        print(sonastik)
+    elif v.get() == 2:
+        print("vist töötab")
+        sonastik = sonastik_keskmine
+        print(sonastik)
+    elif v.get() == 3:
+        return
+        #ei muuda midagi
+    #teen tahvli tühjaks
+    tahvel.delete("all")
+    nupp.place_forget()
+    global nupp1
+    global nupp2
+    global nupp3
+    nupp1.destroy()
+    nupp2.destroy()
+    nupp3.destroy()
+    põhi_programm()
+
+# loome akna
+raam = Tk()
+raam.title("Aminohapete mäng")
+tahvel = Canvas(raam, width= 550, height= 300)
+tahvel.grid()
+
+#aminohappe pilt tahvlile
+#normaal_suurus = Image.open("pildid/aminohape.png")
+#muudetud = normaal_suurus.resize((200,150),Image.ANTIALIAS)
+#aminohape = ImageTk.PhotoImage(muudetud)
+#pilt = tahvel.create_image(175, 100, anchor = W, image= aminohape)
+
+#radiobutton
+v = IntVar()
+#IntVar (või StringVar) väärtuseks on hetkel valitud radiobuttoni value.
+#Selle järgi saab kindlaks teha, milline on valitud.
+#Submit nupp on tavaline nupp. Peale submit vajutamist kontrollid IntVar väärtust.
+
+#Iga nupp on kahel real, et saaksin kasutada destroy() käsku
+nupp1 = Radiobutton(raam, text="Kerge", variable=v, value=1)
+nupp1.grid(row=1)
+nupp2 = Radiobutton(raam, text="Keskmine", variable=v, value=2)
+nupp2.grid(row=2)
+nupp3 = Radiobutton(raam, text="Raske", variable=v, value=3)
+nupp3.grid(row=3)
+
+# loome nupu
+nupp = ttk.Button(raam, text="Valmis", command=raskusaste)
+#Button(raam, text="Tervita!", command=lambda: tervita(argument))
+nupp.place(x=200, y=200, width=150)
+
+# ilmutame akna ekraanile
+raam.mainloop()
+
+
+
