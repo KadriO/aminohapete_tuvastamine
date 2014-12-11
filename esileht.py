e91231a3619a270d7f116cdd409b94cc5e8ee617
AhtoS <ahtosalumets@hotmail.com>
2014-12-11 10:54:53 +0200

    funkstioon peaks töötama

--- esileht.py
+++ esileht.py
@@ -13,18 +13,20 @@ tahvel.grid()
 
 suur_font = font.Font(family='Times', size=14, weight='bold')
 
-silt1 = ttk.Label(raam, text="Oled avanud aminohapete mängu!", font=suur_font, background= "white")
-silt1.place(x=110,y=1)
-silt2 = ttk.Label(raam, text= "Mängida on võimalik erinevatel raskustasemetel. Kui soovid valida valikvastustega versiooni, "
-                              , background= "white")
-silt2.place(x=30, y=30)
-silt3 = ttk.Label(raam, text= "vajuta nuppu xxx. Soovides mängida xxx, vajuta nupule xxx", background="white")
-silt3.place(x=80, y=50)
+silt1 = ttk.Label(raam, text="Aminohapete mäng", font=suur_font, background= "white")
+silt1.place(x=200,y=30)
+silt2 = ttk.Label(raam, text= "Mängida on võimalik nii sisestades aminohappe nime (kolme erineva raskustasemega), kui \
+ka valides kolme erineva valikvastuse vahel. Klikates nuppu \"Valikvastused\" alustad valikvastustega mängu mängimist\
+ ning klikates nuppu \"Tekstisisestus\" alustad mängu, kus tuleb trükkida õige aminohappe nimi. "
+                              , background= "white", wraplength = 500)
+silt2.place(x=30, y=70)
+#silt3 = ttk.Label(raam, text= "vajuta nuppu xxx. Soovides mängida xxx, vajuta nupule xxx", background="white")
+#silt3.place(x=80, y=50)
 
-vasak_nupp = ttk.Button(raam, text="Vajuta mind", command= fn_valikvastused)
-vasak_nupp.place(x= 50, y= 150, width=100)
+vasak_nupp = ttk.Button(raam, text="Valikvastused", command= fn_valikvastused)
+vasak_nupp.place(x= 150, y= 160, width=100)
 
-parem_nupp = ttk.Button(raam, text="Vajuta mind ka", command= raskusastmed_fn)
-parem_nupp.place(x= 200, y= 150, width=100)
+parem_nupp = ttk.Button(raam, text="Tekstisisestus", command= raskusastmed_fn)
+parem_nupp.place(x= 300, y= 160, width=100)
 
 raam.mainloop()
